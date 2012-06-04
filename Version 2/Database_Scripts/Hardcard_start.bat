8d45ca35cb8207521bc497835a9786f56e3a5611
Eugene Ponomarev <eponomarev@yahoo.com>
2012-06-04 13:25:06 -0400

    Hardcard_start.bat update
    setup.exe - commented

--- Version 2/Database_Scripts/Hardcard_start.bat
+++ Version 2/Database_Scripts/Hardcard_start.bat
@@ -1,6 +1,9 @@
-rem Is used to drop Hardcard Db, create Hardcard Db, run Hardcard.sql to create and populate Hardcard Db tables, execute setup.exe 
-rem If SQLServer name is MY-SQLSERVER and Hardcard.sql&setup.exe are in the current directory then correct syntax example :
-rem Hardcard_start.bat MY-SQLSERVER\sqlexpress . (it is .)
+@echo off
+rem drop and create Hardcard Database. Create database schema and populate it with recirds  (, execute setup.exe - commented)
+@echo off
+rem  If SQLServer name is MY-SQLSERVER and Hardcard*.sql files and setup.exe are in the current directory then correct syntax example : 
+@echo off
+rem  Hardcard_start.bat MY-SQLSERVER . (it is .)
 
 :exit(select 100)
 @echo off
@@ -16,10 +19,18 @@ sqlcmd -e -S %1\sqlexpress -Q "drop database Hardcard"
 sqlcmd -e -S %1\sqlexpress -Q "create database Hardcard"
 if errorlevel 1 goto on_sqlcmd2_error
 
-sqlcmd -e -S %1\sqlexpress -d Hardcard -i "%2"\Hardcard.sql
+sqlcmd -S %1\sqlexpress -d Hardcard  -i %2\drop_triggers_for_SQL_EX.sql
+if errorlevel 1 goto on_sqlcmd3_error
+sqlcmd -S %1\sqlexpress -d Hardcard  -i %2\drop_tables_for_SQL_EX.sql
+if errorlevel 1 goto on_sqlcmd3_error
+sqlcmd -S %1\sqlexpress -d Hardcard  -i %2\create_tables_for_SQL_EX.sql
+if errorlevel 1 goto on_sqlcmd3_error
+sqlcmd -S %1\sqlexpress -d Hardcard  -i %2\create_triggers_for_SQL_EX.sql
+if errorlevel 1 goto on_sqlcmd3_error
+sqlcmd -S %1\sqlexpress -d Hardcard  -i %2\create_records_for_SQL_EX.sql
 if errorlevel 1 goto on_sqlcmd3_error
 
-%2\setup.exe
+rem %2\setup.exe
 if errorlevel 1 goto on_setup_error
 
 goto bat_end
