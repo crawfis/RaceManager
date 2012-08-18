5f258d9aef7673b2a55bb2577435ad1a9af8ef29
Roger Crawfis <crawfis.3@osu.edu>
2012-08-18 10:12:01 -0400

    Added files moved to EventProject

---
+++ Version 1/HardCardTests/EventProject/Config.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Hardcard.Scoring
+{
+    public class Config
+    {
+        public int SleepTime = 1000;
+        public int PortNumber = 3900;
+        //public String UserKeyString = "default";
+        public string DataDirectory = "C://Hardcard//";
+        
+        private static Config instance;
+
+        public static Config Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Config();
+                return instance;
+            }
+        }
+
+        private Config()
+        {
+        }
+    }
+}
---
+++ Version 1/HardCardTests/EventProject/PhoneNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hardcard.Scoring
+{
+    /// <summary>
+    /// This struct represents a phone number.
+    /// </summary>
+    [Serializable]
+    public struct PhoneNumber
+    {
+        public String Number { get; set; }
+
+        public PhoneNumber(String pn)
+            : this()
+        {
+            this.Number = pn;
+        }
+
+        public override String ToString()
+        {
+            return Number;
+        }
+    }
+}
