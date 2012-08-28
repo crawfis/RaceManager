e2e2f5cfca6c5c40b1685fdfdb97b63e9695b113
Eugene Ponomarev <eponomarev@yahoo.com>
2012-08-28 15:07:13 -0400

    update for 2012/07/25

--- Version 2/Hardcard_Client/Presenters/RacePresenter.cs
+++ Version 2/Hardcard_Client/Presenters/RacePresenter.cs
@@ -19,23 +19,14 @@ namespace RacingEventsTrackSystem.Presenters
         private readonly ApplicationPresenter _applicationPresenter;
         private Shell _view;
 
-        //private ObservableCollection<Passing> _passingsList;
         private ObservableCollection<String> _eventList;
-        //private ObservableCollection<long> _sessionList;
         private bool _manualPassing = false;
         private String _statusText;
-
-        //private long currentSession;
-        //private String currentEvent;
 
         private HardcardServer server;
         private TagSubscriber passingsLogger;
 
         private Object lockObject;
-
-        //private long lastPassing;
-        //private Thread passingsThread;
-        //private bool stopThread = false;
 
         // TODO put this in a config
         private int networkPort = 3900;
@@ -81,29 +72,6 @@ namespace RacingEventsTrackSystem.Presenters
             }
         }
 
-        /*
-        public ObservableCollection<long> SessionList
-        {
-            get { return _sessionList; }
-            set
-            {
-                _sessionList = value;
-                OnPropertyChanged("SessionList");
-            }
-        }
-        */
-
-        /*
-        public ObservableCollection<Passing> PassingsList
-        {
-            get { return _passingsList; }
-            set
-            {
-                _passingsList = value;
-                OnPropertyChanged("PassingsList");
-            }
-        }
-        */
         public string StatusText
         {
             get { return _statusText; }
@@ -124,16 +92,6 @@ namespace RacingEventsTrackSystem.Presenters
             }
         }
 
-        /*
-        public void EventSelected()
-        {
-            //currentEvent = _view.eventComboBox.SelectedValue.ToString();
-            IQueryable<long> sessions =
-                from p in hardcardContext.Sessions
-                select p.Id;
-            SessionList = new ObservableCollection<long>(sessions.ToList());
-        }
-        */
         public void StartRace()
         {
 
@@ -184,104 +142,5 @@ namespace RacingEventsTrackSystem.Presenters
             server.End();
             StatusText = "Hardcard Server Stopped";
         }
-
-        /* Rmoved as obsolete on 2012/08/17. Just double check if it can be reused
-         
-        public void SessionSelected()
-        {
-            //currentSession = (long) long.Parse(_view.sessionComboBox.SelectedValue.ToString());
-        }
-
-        public void FakeStartRace()
-        {
-            int numpassings = 0;
-            stopThread = false;
-            numpassings = (from p in hardcardContext.Passings
-                           select p.Id).Count();
-            if (numpassings > 0)
-            {
-                lastPassing =
-                    (from p in hardcardContext.Passings
-                     select p.Id).Max();
-            }
-            else
-            {
-                lastPassing = 0;
-            }
-            IQueryable<Passing> passings =
-                from p in hardcardContext.Passings
-                where p.Id > lastPassing
-                select p;
-            PassingsList = new ObservableCollection<Passing>(passings.ToList());
-            StatusText = "Race Started";
-
-            passingsThread = new Thread(FakePassings);
-            passingsThread.Start();
-        }
-
-        public void FakeStopRace()
-        {
-            stopThread = true;
-            passingsThread.Join();
-            StatusText = "Race Stopped";
-        }
-
-        public void FakePassings()
-        {
-            int racetime = 0;
-            short lap = 0;
-            short laptime = 4;
-            int id;
-            while (!stopThread)
-            {
-                Thread.Sleep(1000);
-                for (id = 10000; id < 10002; id++)
-                {
-                    Passing newPassing = new Passing();
-                    newPassing.RFID = id;
-                    newPassing.SessionId = 1;
-                    racetime = racetime + id - 10000 + (laptime % 3);
-                    newPassing.RaceTime = racetime;
-                    //newPassing.LapTime = id - 10000 + (laptime%3);
-                    //newPassing.Lap = lap;
-                    hardcardContext.AddToPassings(newPassing);
-                    hardcardContext.SaveChanges();
-                }
-                IQueryable<Passing> passings = from p in hardcardContext.Passings
-                                               where p.Id > lastPassing
-                                               orderby p.Id descending
-                                               select p;
-                PassingsList = new ObservableCollection<Passing>(passings.ToList());
-                lap++;
-            }
-        }
-
-        public void AddNewPassing()
-        {
-            ManualPassing = true;
-        }
-        public void SaveRace<T>(Session session, PresenterBase<T> presenter)
-        {
-
-            //if (CurrentEcent.Contains(competitor))
-            //    AllCompetitors.Remove(competitor);
-            //_competitorRepository.Delete(competitor);
-
-            MessageBox.Show("N/E");
-            StatusText = string.Format("Event '{0}' was deleted.", session.EventClassId);
-        }
-
-        public void DeleteRace(Session session)
-        {
-
-            //if (CurrentEcent.Contains(competitor))
-            //    AllCompetitors.Remove(competitor);
-            //_competitorRepository.Delete(competitor);
-
-            MessageBox.Show("N/E");
-            StatusText = string.Format("Event '{0}' was deleted.", session.StartTime);
-        }
-
-        */
     }
 }
